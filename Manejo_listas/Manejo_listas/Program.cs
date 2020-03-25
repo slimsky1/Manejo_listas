17352c7dfed8e92f1ed588599d18bd47f35bb7d7
slimsky1 <40033936+slimsky1@users.noreply.github.com>
2020-03-25 15:12:22 -0300

    Init Commit
    Resolución de ejercicio de listas.

---
+++ Manejo_listas/Manejo_listas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Manejo_listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string> { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+            List<string> removeColors = new List<string> { "RED", "WHITE", "BLUE" };
+
+            
+
+            foreach(string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            var listaAux = colors.Except(removeColors).ToList();
+            colors.Clear();
+            colors.AddRange(listaAux);
+            Console.WriteLine();
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        } 
+    }
+}
